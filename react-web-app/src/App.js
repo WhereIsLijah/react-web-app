6636a76145cfb65225caca60b0f00c7f71799324
WhereIsLijah <ayomideoduba@gmail.com>
2023-06-19 13:54:09 -0400

    Added Footer component
    Creation of Footer Component on Homepage

--- react-web-app/src/App.js
+++ react-web-app/src/App.js
@@ -5,6 +5,11 @@ import Button from './components/Button';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/pages/Home'
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import About from './components/pages/About';
+import Cards from './components/Cards';
 
 function App() {
   return (
@@ -12,9 +17,20 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home}/>
+        <Route path='/about' exact Component={About}/>
+        <Route path='/products' exact Component={Products}/>
+        <Route path='/services' exact Component={Services}/>
+        <Route path='/sign-up' exact Component={SignUp}/>
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+
+// Make watch trailer play video in Background
+// change the menu display
+// increase width between Cards
+// create Sign up Form
+//
